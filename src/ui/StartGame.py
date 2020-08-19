e0e9bab1da5f336b571f967e7920010544313513
sean <sean.actor@gmail.com>
2020-08-19 16:07:44 -0700

    Fixed Bugs in StartGame.py

--- src/ui/StartGame.py
+++ src/ui/StartGame.py
@@ -6,26 +6,45 @@ def start_game():
     print("Welcome to Hangman, created by Sean Kyer")
     print("github.com/SeanKyer")
     print("")
+    initialize_game()
 
-    state = True
 
-    print("Please enter the word for your partner to guess!")
-    chosen_word = input("Word: ")
-    user_word = HangmanWord(chosen_word)
-    game = GameInstance(user_word)
+def initialize_game():
+    state_two = True
 
-    count = 0
-    for count in range(0, 50):
-        print(" ")
-        count = count + 1
+    while state_two:
 
-    while state:
-        guess = input("Input Guess! ")
-        game.guess(guess)
-        if game.check_game_over():
-            print("Thanks for playing!")
-            state = False
-        if game.check_victory():
-            print("You Win!")
-            state = False
+        state = True
 
+        print("Please enter the word for your partner to guess!")
+        chosen_word = input("Word: ")
+        user_word = HangmanWord(chosen_word)
+        game = GameInstance(user_word)
+
+        count = 0
+        for count in range(0, 50):
+            print(" ")
+            count = count + 1
+
+        print(game.letters_shown)
+        while state:
+            guess = input("Input Guess! ")
+            game.guess(guess)
+            if game.check_game_over():
+                print("The word was:")
+                print(game.hangman_word.word)
+                print("Thanks for playing!")
+                state = False
+
+            if game.check_victory():
+                print("You Win!")
+                state = False
+
+        choice = input("Would you like to play again? y/n")
+        if choice == "y":
+            count2 = 0
+            for count2 in range(0, 50):
+                print(" ")
+                count = count + 1
+        else:
+            state_two = False
